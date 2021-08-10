f491301679128083fe801bb357a06d16ca9d00bd
Gonzalo Liano ricoy <goliano-@c1r1s1.42madrid.com>
2021-08-10 12:05:38 +0200

    Libft 42

--- ft_substr.c
+++ ft_substr.c
@@ -6,28 +6,48 @@
 /*   By: goliano- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 13:55:20 by goliano-          #+#    #+#             */
-/*   Updated: 2021/08/09 16:54:41 by goliano-         ###   ########.fr       */
+/*   Updated: 2021/08/10 12:01:11 by goliano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	get_length(char const *s, unsigned int start)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[start] != '\0')
+	{
+		i++;
+		start++;
+	}
+	return (i);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	l;
 
 	if (!s)
 		return (NULL);
-	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (start + len > ft_strlen(s))
+		substr = (char *)ft_calloc((get_length(s, start) + 1), sizeof(char));
+	else
+		substr = (char *)malloc((len + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
 	if (start > ft_strlen(s))
 		return (substr);
 	i = 0;
-	while (len--)
+	l = ft_strlen(s);
+	while (i < len && l > 0)
 	{
 		substr[i] = s[start + i];
 		i++;
+		l--;
 	}
 	substr[i] = '\0';
 	return (substr);
